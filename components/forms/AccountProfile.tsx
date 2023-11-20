9e7baa60a7a2139eeda63a8cd7268c80735621e5
David <david.estrelloso.tribugenia@gmail.com>
2023-11-21 07:41:04 +0800

    Finished MongoDB connection and Onboarding Functionalities

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,8 @@ import { ChangeEvent, useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 interface Props {
   user: {
     id: string;
@@ -33,6 +35,9 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
+
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
@@ -65,20 +70,31 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     }
   };
   const onSubmit = async (values: z.infer<typeof UserValidation>) => {
-    console.log(values);
-    // const blob = values.profile_photo;
+    const blob = values.profile_photo;
 
-    // const hasImageChanged = isBase64Image(blob);
+    const hasImageChanged = isBase64Image(blob);
 
-    // if (hasImageChanged) {
-    //   const imgRes = await startUpload(files);
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
 
-    //   if (imgRes && imgRes[0].fileUrl) {
-    //     values.profile_photo = imgRes[0].fileUrl;
-    //   }
-    // }
+      if (imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl;
+      }
+    }
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname,
+    });
 
-    // TODO
+    if (pathname === "/profile/edit") {
+      router.back();
+    } else {
+      router.push("/");
+    }
   };
   return (
     <Form {...form}>
